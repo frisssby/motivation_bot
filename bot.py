13165a403d56666b23792c1474b1a5c4776c34e5
Elina Safarova <elin.safarova2002@yandex.ru>
2021-12-25 19:57:58 +0300

    Try to break CI

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ import telebot
 bot = telebot.TeleBot(os.environ.get('TOKEN'))
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    # """"Send user the greeting message"""
+def SendWelcome(message):
+    """"Send user the greeting message"""
     bot.reply_to(message, "Hi, buddy! Type /quote to get your piece of motivation.")
 
 @bot.message_handler(func=lambda message: True)
