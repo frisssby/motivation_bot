32e8be9aeb7d150ff43df8317a3db187605d4185
Elina Safarova <elin.safarova2002@yandex.ru>
2021-12-25 21:54:23 +0300

    Fix bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,6 @@ def echo_all(message):
     if response:
         bot.send_message(message.chat.id, response.json().get("quote"))
     else:
-        bot.send_message(message.chat.id, "Something went wrong. Please, try again later.") 
+        bot.send_message(message.chat.id, "Something went wrong. Please, try again later.")
         
 bot.infinity_polling()
